cb4673722976c248f954f180df05d9c113c1513c
Miguendes117 <alu.21130557@correo.itlalaguna.edu.mx>
2025-02-12 13:28:19 -0600

    Nuevos cambios Bien cheveres de la clase del 12 de feb del 2225

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.ejemplo1
 
+import android.graphics.drawable.Icon
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
@@ -18,33 +35,111 @@ import androidx. compose. ui. unit. sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Ejemplo1Theme()
+            {
+                GreetingPreview()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        fontSize =  100.sp
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("Jesus Miguel")
-        Greeting("Sonic")
+fun GreetingPreview(){
+    Column {
+        //TextFieldSencillo()
+        //TextFieldPlaceHolder()
+        //TextFieldKeyboard()
+        //OutLineTextFieldSample()
+        TextFieldWithIcons()
     }
 }
+
+
+@Composable
+fun  TextFieldSencillo()
+{
+    var text by remember { mutableStateOf("") }
+    Column{
+        TextField(
+            value = text,
+            onValueChange =  { newText ->
+                text = newText
+            },
+            label = { Text( "introduce tu nombre")}
+        )
+        Text(text = "Tu nombre es:  $text")
+    }
+}
+
+
+@Composable
+fun TextFieldPlaceHolder(){
+    var text by remember{ mutableStateOf("")}
+    Column {
+        TextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+            },
+            label = { Text("nombre")},
+            placeholder = { Text("introduce tu nombre")}
+        )
+        Text (text = "Tu nombre es: $text")
+    }
+}
+
+@Composable
+fun TextFieldKeyboard(){
+    var text by remember { mutableStateOf("") }
+    Column {
+        TextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+            },
+            label = { Text("nombre")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = "Tu numero es:  $text")
+    }
+}
+
+
+@Composable
+fun OutLineTextFieldSample() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    Column {
+        OutlinedTextField(
+            value = text,
+            label = { Text(text = "Enter Your Name") },
+            onValueChange = {
+                text = it
+            }
+        )
+        Text(text = "Tu nombre es :  ${text.text}")
+    }
+}
+
+
+@Composable
+fun TextFieldWithIcons() {
+    var text by remember { mutableStateOf(("")) }
+    Column {
+        return OutlinedTextField(
+            value = text,
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
+            //trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
+            onValueChange = {
+                text = it
+            },
+            label = { Text(text = "Email address") },
+            placeholder = { Text(text = "Enter your e-mail") },
+        )
+        Text(text =  "Tu nombre es: $text")
+    }
+}
+
+
